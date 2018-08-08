c21a16be59f058678f9e5734e17aa56e9fdcefca
Stephen Ford <stephen.ford@digital.cabinet-office.gov.uk>
2018-08-08 12:02:07 +0100

    Update module to align with recent changes in jenkins module
    - removed terraform.tfvars.example as not needed by module
    - renamed variable to make consistent between dns module and jenkins module
    - removed provider configuration as it gets defined in the enveloping terraform code that calls this module
    - removed uneccessary variables (az, region, profile) as not needed in module
    
    Solo: @smford

--- variables.tf
+++ variables.tf
@@ -1,33 +1,9 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = "string"
-  default     = "eu-west-2"
-}
-
-variable "aws_az" {
-  description = "AWS availability zone. Note that this must exist in the region specified in the aws_region variable"
-  type        = "string"
-  default     = "eu-west-2a"
-}
-
-variable "aws_profile" {
-  description = "AWS profile name from ~/.aws/credentials or wherever your AWS credentials are stored"
+variable "hostname_suffix" {
+  description = "Main domain name for new Jenkins instances, e.g. example.com"
   type        = "string"
 }
 
 variable "team_name" {
-  description = "The name of your team"
+  description = "Name of your team. This is used to construct the DNS name for your Jenkins instances"
   type        = "string"
-}
-
-variable "top_level_domain_name" {
-  description = "Top level domain name"
-  type        = "string"
-  default     = "build.gds-reliability.engineering"
-}
-
-variable "resource_prefix" {
-  description = "Prefix for resources created in this module"
-  type        = "string"
-  default     = ""
 }
